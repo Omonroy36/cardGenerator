0a7d506a5b086ebf0b71150b346bf5333d614611
Omonroy36 <monroyorlando.36@gmail.com>
2019-07-13 00:57:06 +0000

    practice

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,25 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  document.getElementById("excuse").innerHTML =
+    "<p>" +
+    who[getRandomInt(0, 4)] +
+    " " +
+    what[getRandomInt(0, 4)] +
+    " " +
+    when[getRandomInt(0, 5)] +
+    "</p>";
 };
